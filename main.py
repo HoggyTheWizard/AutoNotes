a627aeea90562b1258245aa15a1ad6a73f398c15
HoggyTheWizard <hogwartswizrd@icloud.com>
2021-10-28 11:19:03 -0400

    Remove unneeded function

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 import wikipedia
 
 
-def insert_newlines(string, every):  # function taken from stackoverflow
-    return '\n'.join(string[i:i+every] for i in range(0, len(string), every))
-
-
 with open("terms.txt") as file:
     term_list = []
     for word in file.readlines():
@@ -13,7 +9,7 @@ with open("terms.txt") as file:
 
     for term in term_list:
         try:
-            print(f"{term[1]} - {insert_newlines(wikipedia.page(term[0]).summary[:500], every=200)}\n\n")
+            print(f"{term[1]} - {wikipedia.page(term[0]).summary[:500]}\n\n")
         except:
             print(f"ERROR > term \"{term[0]}\" not found. Manual labor time.\n\n")
 
