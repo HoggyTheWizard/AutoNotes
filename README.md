d9561709e8156ccea8bffa74c5c5449d14476796
marzeq <marzeqmarzeq@gmail.com>
2022-01-07 01:06:53 +0100

    Make a bunch of improvements - You can now change the input filename - The script puts all the results in a seperate textfile - You can use the -I (--interactive) flag that allows you to change your term if it couldn't be found And some other minor ones.

--- README.md
+++ README.md
@@ -2,8 +2,9 @@
 A personal project I'm making so that I don't have to waste my time doing note assignments.
 
 For those of you who want to use it:
-1. Create file `terms.txt`
-2. Put your terms in there, each term on a newline
-3. Avoid descriptions.  If your term is "John Adams, President of the United States", make it "John Adams"
-4. This is a quick script I made for my own personal use. Unless something is an easy fix, I likely won't be changing the 
-code to support new things.
+1. Make a file called something like `terms.txt`
+2. Put your terms in there, each term on a newline (Avoid descriptions. If your term is "John Adams, President of the United States", make it "John Adams")
+3. Run the script like this: `python main.py -i <your input file> -o <output filename>`.
+4. Optionally, you can use the `-I` (`--interactive`) flag that allows you to correct terms that couldn't be found/were too ambiguous.
+
+Note: This is a quick script I made for my own personal use. Unless something is an easy fix, I likely won't be changing the code to support new things.
--- main.py
+++ main.py
@@ -1,23 +1,52 @@
 import wikipedia
+import argparse
 
+parser = argparse.ArgumentParser(description="Automatically search Wikipedia for provided terms")
+parser.add_argument("--input", "-i", type=str, help="Input file containing terms to search for", required=True)
+parser.add_argument("--output", "-o", type=str, help="Output file to write results to", required=True)
+parser.add_argument("--interactive", "-I", action="store_true", help="Interactive mode")
+args = parser.parse_args()
 
-with open("terms.txt") as file:
+def interactive_try_until_found(initial_error, initial_term):
+    new_term = initial_term
+    print(f"{new_term} - {initial_error}")
+    while True:
+        new_term = input("Try to find a new term: ")
+        try:
+            new_term = new_term.replace("\"", "").replace(",", "").replace("\n", "")
+            search_term = new_term.replace(" ", "_").lower()
+            raw_text = wikipedia.summary(search_term)[:500]
+            text = raw_text.rsplit(".", 1)[0]
+            print(f"{term[1]} - {text}.")
+            return f"{new_term} - {text}.\n"
+        except wikipedia.exceptions.PageError:
+            print(f"{new_term} - Not found.")
+        except wikipedia.exceptions.DisambiguationError:
+            print(f"{new_term} - Too ambiguous.")
+
+with open(args.input) as file:
     term_list = []
     for word in file:
-        word.replace("\n", "")
-        word.replace("\"", "")
-        word.replace(",", "")
+        word = word.replace("\n", "").replace("\"", "").replace(",", "")
         term_list.append([word.replace(" ", "_").lower(), word])
 
-    for term in term_list:
-        try:
-            raw_text = wikipedia.page(term[0]).summary[:500]
-            text = raw_text.split(".")
-            text.remove(text[-1])
-            text.reverse()
-            formatted_text = "".join(text)
-            print(f"{term[1]} - {formatted_text}.")
-        except:
-            continue
+output_text = ""
+
+for term in term_list:
+    try:
+        raw_text = wikipedia.summary(term[0])[:500]
+        text = raw_text.rsplit(".", 1)[0]
+        print(f"{term[1]} - {text}.")
+        output_text += f"{term[1]} - {text}.\n"
+    except wikipedia.exceptions.PageError:
+        if args.interactive:
+            output_text += interactive_try_until_found("Not found.", term[1])
+    except wikipedia.exceptions.DisambiguationError:
+        if args.interactive:
+            output_text += interactive_try_until_found("Too ambiguous.", term[1])
 
     print("Finished!")
+
+with open(args.output, "w") as file:
+    file.write(output_text)
+
